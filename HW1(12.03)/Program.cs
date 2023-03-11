6c0b6d1b3da59282ffb47fcdaad252928bba8d75
voidstuck <voidstuck@yandex.com>
2023-03-11 23:36:46 +0700

    First homework

---
+++ HW1(12.03)/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите Ваше имя");
+        string yourName = Console.ReadLine();
+        Console.WriteLine($"{yourName}, привет!");
+        
+    }
+}
+
