bee49c9a0e7db7742bb1ddc49df3140803bdd936
voidstuck <voidstuck@yandex.com>
2023-04-09 22:48:38 +0700

    Added homework 5 (files and directories)

---
+++ HW5/ItemsForRecord.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW5_prog1
+{
+    public record class ItemsForRecord(ItemsForRecord.Types Type, string Name, DateTime LastDateTime)
+    {
+        public enum Types
+        {
+            File = 1,
+            Directory = 2,
+            Unknown = 3
+        }
+        public Types Type { get; set; } = Type;
+        public string Name { get; set; } = Name;
+        public DateTime LastDatetime { get; set; } = LastDateTime;
+        
+    }
+}
